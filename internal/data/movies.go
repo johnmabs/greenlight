33ba5a8e7f44973b30725889536c8c6f0c5ea47c
John Mabiala <johnmabiala@hotmail.com>
2026-02-11 18:59:27 +0100

    Validating JSON Input

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,20 +2,33 @@ package data
 
 import (
 	"time"
+
+	"greenlight.johnmabiala.net/internal/validator" // New import
 )
 
-// Annotate the Movie struct with struct tags to control how the keys appear in the
-// JSON-encoded output.
 type Movie struct {
 	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"-"` // Use the - directive
+	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
-	Year      int32     `json:"year,omitzero"` // Add the omitzero directive
-	// Use the Runtime type instead of int32. Note that the omitzero directive will
-	// still work on this: if the Runtime field has the underlying value 0, then it will
-	// be considered zero and omitted -- and the MarshalJSON() method we just made
-	// won't be called at all.
-	Runtime Runtime  `json:"runtime,omitzero"`
-	Genres  []string `json:"genres,omitempty"` // Use the omitempty directive
-	Version int32    `json:"version"`
+	Year      int32     `json:"year,omitzero"`
+	Runtime   Runtime   `json:"runtime,omitzero"`
+	Genres    []string  `json:"genres,omitzero"`
+	Version   int32     `json:"version"`
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
