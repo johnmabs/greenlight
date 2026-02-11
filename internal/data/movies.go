37e64b6444a9375610727c4f13912377d8c8f905
John Mabiala <johnmabiala@hotmail.com>
2026-02-11 17:28:02 +0100

    Advanced JSON Customization

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -10,8 +10,12 @@ type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"` // Use the - directive
 	Title     string    `json:"title"`
-	Year      int32     `json:"year,omitzero"`           // Add the omitzero directive
-	Runtime   int32     `json:"runtime,omitzero,string"` // Add the string directive
-	Genres    []string  `json:"genres,omitempty"`        // Use the omitempty directive
-	Version   int32     `json:"version"`
+	Year      int32     `json:"year,omitzero"` // Add the omitzero directive
+	// Use the Runtime type instead of int32. Note that the omitzero directive will
+	// still work on this: if the Runtime field has the underlying value 0, then it will
+	// be considered zero and omitted -- and the MarshalJSON() method we just made
+	// won't be called at all.
+	Runtime Runtime  `json:"runtime,omitzero"`
+	Genres  []string `json:"genres,omitempty"` // Use the omitempty directive
+	Version int32    `json:"version"`
 }
