f5a1fcf6fe64ee429d126651f4b517825f8c6aec
John Mabiala <johnmabiala@hotmail.com>
2026-02-11 17:38:30 +0100

    Sending Error Messages

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -21,7 +21,8 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	id, err := app.readIDParam(r)
 
 	if err != nil {
-		http.NotFound(w, r)
+		// Use the new notFoundResponse() helper.
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -34,11 +35,9 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		Version:   1,
 	}
 
-	// Create an envelope{"movie": movie} instance and pass it to writeJSON(), instead
-	// of passing the plain movie struct.
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		// Use the new serverErrorResponse() helper.
+		app.serverErrorResponse(w, r, err)
 	}
 }
