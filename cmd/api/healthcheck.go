33a87b87978199d92f6996245bb0cfadc618b4a7
John Mabiala <johnmabiala@hotmail.com>
2026-02-11 17:17:13 +0100

    Formatting and Enveloping Responses

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,14 +6,17 @@ import (
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 
-	// Create a map which holds the information that we want to send in the response.
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	// Declare an envelope map containing the data for the response. Notice that the way
+	// we've constructed this means the environment and version data will now be nested
+	// under a system_info key in the JSON response.
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
-
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Error(err.Error())
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
