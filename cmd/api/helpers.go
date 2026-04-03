95317373fb18af0ae758222fd61d8e5a2c960282
John Mabiala <johnmabiala@hotmail.com>
2026-04-03 20:33:18 +0100

    sending json responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"maps"
 	"net/http"
 	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+// Define an envelope type.
+type envelope map[string]any
 
 // Retrieve the "id" URL parameter from the current request context, then convert it to
 // an integer and return it. If the operation isn't successful, return 0 and an error.
@@ -19,4 +24,25 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+// helper for sending responses.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	// Encode the data to JSON, returning the error if there was one.
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	maps.Copy(w.Header(), headers)
+
+	// Add the "Content-Type: application/json" header, then write the status code and
+	// JSON response.
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module greenlight.johnmabiala.net
 
 go 1.26.1
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require github.com/julienschmidt/httprouter v1.3.0
